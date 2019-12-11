8fbedb5387a89d9f90588eca440f82ab462f9979
Garvit karnwal <garvitkarnwal@gmail.com>
2019-12-11 23:21:21 +0530

    test commit

--- ProductImporter/Model/CapterraFeedModel.cs
+++ ProductImporter/Model/CapterraFeedModel.cs
@@ -5,7 +5,7 @@ using System.Text;
 namespace ProductImporter.Model
 {
     class CapterraFeedModel
-    {
+    { 
         public string tags { get; set; }
         public string name { get; set; }
         public string twitter { get; set; }
